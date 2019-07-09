8ef05d6ca632de3e08b23e38c2d77967575821c1
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-09 18:44:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,44 +1,46 @@
 class Song
-  extend Concerns::Findable
   attr_accessor :name, :artist, :genre
 
   @@all = []
 
-  def initialize(name, artist= nil, genre= nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist= (artist) unless artist == nil
-    self.genre= (genre) unless genre == nil
-    save
+    self.artist=artist if artist
+    self.genre=genre if genre
   end
 
   def self.all
-    @@all 
+    @@all
   end
 
   def self.destroy_all
-    self.all.clear
+    @@all.clear
   end
 
   def save
     @@all << self
   end
-  
-  def self.create(song, artist=nil, genre=nil)
-    new_song = Song.new(song, artist, genre)
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
   end
-  
-  def artist= (artist)
+
+  def artist=(artist)
     @artist = artist
-    self.artist.add_song(self)
+    artist.add_song(self)
   end
-  
-  def genre= (genre)
+
+  def genre=(genre)
     @genre = genre
-    self.genre.add_song(self)
+    if !(genre.songs.include?(self))
+      genre.songs << self
+    end
   end
 
   def self.find_by_name(name)
-    all.find {|song| song.name == name}
+    all.detect {|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
@@ -48,17 +50,13 @@ class Song
   def self.new_from_filename(filename)
     info = filename.split(" - ")
     artist, name, genre = info[0], info[1], info[2].gsub( ".mp3" , "")
+
     genre = Genre.find_or_create_by_name(genre)
     artist = Artist.find_or_create_by_name(artist)
     new(name,artist,genre)
   end
 
-  def self.find_or_create_by_name(song_name)
-    song = self.find_by_name(song_name)
-    if song == nil
-      self.create(song_name)
-    else
-      song
-    end
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save}
   end
 end
