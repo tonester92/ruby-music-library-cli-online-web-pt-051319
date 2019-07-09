073d5e989f689a7888679d5ed1d823bf17ed4e0a
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-09 18:45:53 +0000

    Automatically backed up by Learn

--- lib/music_libary_controller.rb
+++ lib/music_libary_controller.rb
@@ -58,7 +58,6 @@ class MusicLibraryController
     genres.each.with_index(1) {|genre, i| puts "#{i}. #{genre.name}"}
   end
 
-  # binding.pry
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
